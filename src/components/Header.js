74dc0f2558a7f6f1f1ea5adcb846a84783f05ca5
Aidan Gilmore <aidangilmore@gmail.com>
2020-09-07 22:07:36 +0200

    hi# Changes to be committed:

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,64 +1,84 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Header = props => (
-  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
-    <div className="logo">
-      <span className="icon fa-diamond"></span>
-    </div>
-    <div className="content">
-      <div className="inner">
-        <h1>Dimension</h1>
-        <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
-        </p>
-      </div>
-    </div>
-    <nav>
-      <ul>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('intro')
-            }}
-          >
-            Intro
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('work')
-            }}
-          >
-            Work
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('about')
-            }}
-          >
-            About
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('contact')
-            }}
-          >
-            Contact
-          </button>
-        </li>
-      </ul>
-    </nav>
-  </header>
+const Header = props => ( <
+  header id = "header"
+  style = {
+    props.timeout ? {
+      display: 'none'
+    } : {}
+  } >
+  <
+  div className = "logo" >
+  <
+  span className = "icon fa-diamond" > < /span> < /
+  div > <
+  div className = "content" >
+  <
+  div className = "inner" >
+  <
+  h1 > Aidan Gilmore < /h1> <
+  p >
+  A fully trained web developer by {
+    '@'
+  } <
+  a href = "https://upleveled.io/" > Upleveled < /a> and prepared <
+  br / >
+  to work
+  for free under the {
+    ' '
+  } <
+  a href = "https://www.ams.at/" > AMS < /a> protocol. < /
+  p > <
+  /div> < /
+  div > <
+  nav >
+  <
+  ul >
+  <
+  li >
+  <
+  button onClick = {
+    () => {
+      props.onOpenArticle('intro')
+    }
+  } >
+  Intro <
+  /button> < /
+  li > <
+  li >
+  <
+  button onClick = {
+    () => {
+      props.onOpenArticle('work')
+    }
+  } >
+  Work <
+  /button> < /
+  li > <
+  li >
+  <
+  button onClick = {
+    () => {
+      props.onOpenArticle('about')
+    }
+  } >
+  About <
+  /button> < /
+  li > <
+  li >
+  <
+  button onClick = {
+    () => {
+      props.onOpenArticle('contact')
+    }
+  } >
+  Contact <
+  /button> < /
+  li > <
+  /ul> < /
+  nav > <
+  /header>
 )
 
 Header.propTypes = {
